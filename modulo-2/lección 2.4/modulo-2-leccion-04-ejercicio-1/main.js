c41d918075c7546c83a2099090a8a23ac2d41541
Rocío Garrido <rocioganon@gmail.com>
2021-05-06 10:18:24 +0200

    change ex 1

--- modulo-2/lección 2.4/modulo-2-leccion-04-ejercicio-1/main.js
+++ modulo-2/lección 2.4/modulo-2-leccion-04-ejercicio-1/main.js
@@ -1,7 +1,9 @@
-// Crea una función que reciba como argumentos dos valores y devuelva como valor de retorno la multiplicación de ambos. Haz tres pruebas con distintos números para comprobar que funciona correctamente y muestra el resultado en la consola usando console.log().
+// Haz tres pruebas con distintos números para comprobar que funciona correctamente. 
 
 let number1 = 754;
 let number2 = 23;
+
+// Crea una función que reciba como argumentos dos valores y devuelva como valor de retorno la multiplicación de ambos.
 
 function multiply (number1,number2) {
     return number1 * number2;
@@ -9,4 +11,5 @@ function multiply (number1,number2) {
 
 const result = multiply(number1,number2);
 
+// Muestra el resultado en la consola usando console.log().
 console.log(result);
